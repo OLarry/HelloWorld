cb30b1f42f39301e03b2dbc31a16da7f849f3282
larryoshea <larry.oshea.larry@gmail.com>
2014-09-20 02:08:17 +0300

    Book check activity

--- app/src/main/java/com/example/larryoshea/helloworld/MyActivity.java
+++ app/src/main/java/com/example/larryoshea/helloworld/MyActivity.java
@@ -2,20 +2,45 @@ package com.example.larryoshea.helloworld;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MyActivity extends Activity {
 
+    String tag = "lifecycle";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
-
-
     }
 
+    public void onStart() {
+        super.onStart();
+        Log.d(tag, "in the onStart() event");
+    }
+
+    public void onRestart() {
+        super.onRestart();
+        Log.d(tag, "in the onRestart() event");
+    }
+
+    public void onPause() {
+        super.onPause();
+        Log.d(tag, "in the onPause() event");
+    }
+
+    public void onResume() {
+        super.onResume();
+        Log.d(tag, "in the onResume() event");
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(tag, "in the onDestroy() event");
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
